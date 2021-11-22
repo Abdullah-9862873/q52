8a1a2d43c786951bcdca89290479748778268744
shahzaibnawaz902@gmail.com <shahzaibnawaz902@gmail.com>
2021-11-22 09:06:27 -0800

    Add project files.

---
+++ Project/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+			//Part a
+			int a = (1 + 8 / 2 + ((1 * 4) + (5 * 4)) / 4);
+			Console.WriteLine(a);
+			Console.ReadLine();
+
+			//Part b
+			int b = ((1 + 1 + 1 + 1) / 2 + (1 + 1 + 1)/3;
+			Console.WriteLine(b);
+			Console.ReadLine();
+
+			//Part c
+			int c = (5 * 5 + 5 / 5 + 6);
+			Console.WriteLine(c);
+			Console.ReadLine();
+
+			//Part d
+			int d = (((3 + 4) + (4 * 7)) / 5);
+			Console.WriteLine(d);
+			Console.ReadLine();
+
+			//Part e
+			int e = ((3 * 6 * 7 * 2) +12 / 2);
+			Console.WriteLine(e);
+			Console.ReadLine();
+		}
+    }
+}
